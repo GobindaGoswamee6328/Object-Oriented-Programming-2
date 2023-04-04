1eca54807029c470fe4979b69cffa1d74747d445
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-04-04 11:35:39 +0600

    Add files via upload

---
+++ LabTask/LabTask/Product.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask
+
+{ 
+     struct ManufacturingDate
+     {
+       private int date;
+       private int month;
+       private int year;
+
+    public ManufacturingDate(int date, int month, int year)
+    {
+
+        this.date = date;
+        this.month = month;
+        this.year = year;
+
+    }
+     public void print()
+    {
+
+        Console.WriteLine("Date : {0}", this.date);
+        Console.WriteLine("Month : {0}", this.month);
+        Console.WriteLine("year : {0}", this.year);
+
+
+    }
+
+
+
+
+}
+  
+   
+
+     class Product
+    {
+        private int id;
+        private string name;
+        private double price;
+        private ManufacturingDate mydate;
+        
+
+
+        public void setid(int id)
+
+        {
+            this.id = id;
+        }
+
+        public int getId()
+        {
+            return this.id;
+                   
+        }
+
+        public void setname(string name)
+
+        {
+            this.name = name;
+        }
+
+        public string getname()
+        {
+            return this.name;
+
+        }
+
+        public void setprice(double price)
+
+        {
+            this.price = price;
+        }
+
+        public double getprice()
+            
+        {
+            return this.price;
+
+        }
+
+     public void setMyDate(ManufacturingDate mydate)
+     {
+        this.mydate = mydate;
+     }
+
+    public ManufacturingDate getMydate()
+     {
+        return this.mydate;
+     }
+
+        public Product()
+        { 
+        
+        }
+
+        public Product(int id, string name, ManufacturingDate mydate,double price)
+        {
+
+            this.setid(id);
+            this.setname(name);
+            this.setMyDate(mydate);
+            this.setprice(price);
+
+        }
+
+
+
+    public void printdata()
+    {
+        Console.WriteLine(" Product Id : {0}", this.id);
+        Console.WriteLine(" Product Name : {0}", this.name);
+        mydate.print();
+        Console.WriteLine(" Product Price : {0}", this.price);
+        Console.WriteLine();
+
+    }
+
+}
+}
---
+++ LabTask/LabTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product s1 = new Product();
+            s1.setid(25);
+            s1.setname("Samim");
+            s1.setMyDate(new ManufacturingDate(13, 2, 2023));
+            s1.setprice(650);
+            s1.printdata();
+
+            Product s2 = new Product( 20, "Simanta", new ManufacturingDate(10, 2, 2023),1000);
+            s2.printdata();
+           
+
+        }
+    }
+}
