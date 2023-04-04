a603c8b768870be711dc8778ecbc304e2c2f5265
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-04-04 16:15:34 +0600

    Add files via upload

---
+++ ConsoleAppClass/ConsoleAppClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClass
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(15, "Pulok", 3.48);
+            s1.setdata();
+
+
+        }
+        
+
+
+
+
+
+    }
+}
---
+++ ConsoleAppClass/ConsoleAppClass/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClass
+{
+     class Student
+    {
+        public int id;
+        public string name;
+        public double cgpa;
+
+        public Student(int id, string name, double cgpa)
+        {
+
+            this.id = id;
+            this.name = name;
+            this.cgpa = cgpa;
+        }
+
+        public void setdata()
+        {
+            Console.WriteLine("Student Id : {0}", this.id);
+            Console.WriteLine("Student Name : {0}", this.name);
+            Console.WriteLine("Student Cgpa : {0}", this.cgpa);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
