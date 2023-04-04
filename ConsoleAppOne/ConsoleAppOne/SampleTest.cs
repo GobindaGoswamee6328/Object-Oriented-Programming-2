97357b92113152ed75c6fcfe393a4bd6c4ad6cf7
Gobinda Goswamee <127843847+GobindaGoswamee6328@users.noreply.github.com>
2023-04-04 20:30:44 +0600

    Add files via upload

---
+++ ConsoleAppOne/ConsoleAppOne/SampleTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOne
+{
+    internal partial class Sample
+    {
+        internal void M2()
+        {
+
+
+
+        }
+    }
+}
